0ec9b39cca5db345ed68aef1a494e77480592ad3
TAugust <taugust@noreply.codeberg.org>
2026-06-29 17:28:23 +0200

    empty test

--- csharp/tests/TruthTableTests/Program.cs
+++ csharp/tests/TruthTableTests/Program.cs
@@ -1,16 +1,2 @@
-﻿public class MyAwesomeTestSuite
-{
-    [Fact]
-    public void MyAwesomeTest()
-    {
-        // Arrange
-        int a = 2;
-        int b = 3;
-
-        // Act
-        int result = a + b;
-
-        // Assert
-        Assert.Equal(5, result);
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
